38e76bc1f68d2a688d444bd8022aca028092b744
tony <anthony@amartinez.cc>
2021-02-10 15:04:48 -0800

    new user and login routes with bcrypt

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,9 +1,11 @@
+const bcrypt = require('bcryptjs')
+
 const db = require('../../model/userModel');
+
 
 const userController = {};
 
 userController.getAll = (req, res, next) =>{
-
     const getAllUsers = 'select * from users';
 
     db.query(getAllUsers)
@@ -17,4 +19,96 @@ userController.getAll = (req, res, next) =>{
         })
 }
 
-module.exports = userController;
+/*
+https://www.npmjs.com/package/bcrypt
+
+ //check is 
+// bcrypt.compare('aaaa', result)
+//   .then(compareRes =>{
+//       console.log('compare: ', compareRes)
+//   })
+
+*/
+
+function insertFunc(first, last, email, pwd){
+    const intoStr = 'insert into users (firstName, lastName, email, pwd) ';
+    const valuesStr = "values (" + "'"+
+                       first + "'"+ "," + "'"+ 
+                       last + "'"+ "," + "'"+ 
+                       email +"'"+ "," + "'"+ 
+                       pwd +"'"+ ");"
+    return intoStr+valuesStr;
+}
+
+userController.signup = (req, res, next) =>{    
+
+    const {firstname, lastname, email, pwd} = req.body;
+    console.log('req body:', req.body)
+    const saltRounds = 10;
+    bcrypt.hash(pwd, saltRounds)
+      .then(result=>{
+        console.log('hash results: ', result);
+        db.query(insertFunc(firstname, lastname, email, result))
+          .then(dbResult => {
+            console.log('inserting query: , ', dbResult);
+            return next();
+        })
+        .catch(e=>{
+            /*
+            todo: if duplicate email address then send email taken 
+                  login instead or use diff email address
+            */
+            console.log('error from db.query: ', e);
+            return next(e);
+        })   
+      })
+      .catch(err =>{
+          console.log('error in bcrypt: ', err);
+          return next(err)
+      });
+
+
+    
+    
+
+    // db.query(insertNewUser)
+    //     .then(data =>{
+    //         console.log('get all query response ', data.rows);
+    //         return next();
+    //     })
+    //     .catch(e => {
+    //         console.error(e.stack);
+    //         return next(e);
+    //     })
+}
+
+userController.login = (req, res, next) =>{    
+    const {email, pwd} = req.body;
+    const getOneUser = "select * from users where email ="+"'"+email+"';";
+    console.log('query: ', getOneUser);
+    console.log('req body:', req.body)
+    
+    db.query(getOneUser)
+      .then(result =>{
+          bcrypt.compare(pwd, result.rows[0].pwd)
+            .then(bcryptRes =>{
+                console.log('login compare: ', bcryptRes)
+                return next();
+            })
+            .catch(e => {
+                        console.error('login error: ', e);
+                        return next(e);
+                    })        
+      })    
+      .catch(err =>{
+          console.log('error in login query: ', err);
+          return next(err)
+      });
+
+
+    
+    
+
+
+}
+module.exports = userController;///
--- server/routes/api.js
+++ server/routes/api.js
@@ -12,18 +12,28 @@ const router = express.Router();
 /**
 login page
 */
-router.get('/users', userController.getAll, (req, res)=> {
-    console.log('in / of /api')
-    res.header('content-type', 'text/html; charset=utf-8');
-    return res.status(200).send('check the console');
-  }
-);
+router.post('/signup', userController.signup, (req, res)=> {
+  console.log('in /signup of /api')
+  res.header('content-type', 'text/html; charset=utf-8');
+  return res.status(200).send('success...signup');
+});
+
+router.post('/login', userController.login, (req, res)=> {
+  console.log('in /signup of /api')
+  res.header('content-type', 'text/html; charset=utf-8');
+  return res.status(200).send('success...login');
+});
+
+router.post('/users', userController.getAll, (req, res)=> {
+  console.log('in / of /api')
+  res.header('content-type', 'text/html; charset=utf-8');
+  return res.status(200).send('check the console');
+});
 
 router.get('/',  (req, res)=> {
-    console.log('in / of /api')
-    res.header('content-type', 'text/html; charset=utf-8');
-    return res.status(200).sendFile(path.resolve(__dirname, '../../client/index.html'));
-  }
-);
+  console.log('in / of /api')
+  res.header('content-type', 'text/html; charset=utf-8');
+  return res.status(200).sendFile(path.resolve(__dirname, '../../client/index.html'));
+});
 
 module.exports = router;
